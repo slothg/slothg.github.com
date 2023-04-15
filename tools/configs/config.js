500571b207a6aedefeaef442b3fb813cec6948de
slothg <slothg@163.com>
2023-04-15 13:34:12 +0800

    vb数据填充生成工具

--- tools/configs/config.js
+++ tools/configs/config.js
@@ -1,74 +1,78 @@
 var conf = [
-	{
-		title: "tools",
-		items: [
-			{
-				text: "bbcode-editor",
-				link: "./devtools/ckeditor/bbcode.html",
-			},
-			{
-				text: "CocosCreator 资源 UUID 解析",
-				link: "./subpages/ccuuid.html"
-			},
-			{
-				text: "InvoluteSeek",
-				link: "./subpages/InvoluteSeek.html"
-			},
-			{
-				text: "line-easing",
-				link: "./subpages/line-easing.html"
-			},
-			{
-				text: "littera",
-				link: "./subpages/littera.html"
-			},
-			{
-				text: "位图字体基础字符",
-				link: "./subpages/fntchars.html"
-			},
-		]
-	},
-	{
-        title: "唤起 app",
+    {
+        title: 'tools',
         items: [
             {
-                text: "ab",
-                link: "ab://123",
-                desc: "",
-            }
+                text: 'bbcode-editor',
+                link: './devtools/ckeditor/bbcode.html',
+            },
+            {
+                text: 'CocosCreator 资源 UUID 解析',
+                link: './subpages/ccuuid.html',
+            },
+            {
+                text: 'AssemblerVB',
+                link: './subpages/AssemblerVB.html',
+            },
+            {
+                text: 'InvoluteSeek',
+                link: './subpages/InvoluteSeek.html',
+            },
+            {
+                text: 'line-easing',
+                link: './subpages/line-easing.html',
+            },
+            {
+                text: 'littera',
+                link: './subpages/littera.html',
+            },
+            {
+                text: '位图字体基础字符',
+                link: './subpages/fntchars.html',
+            },
+        ],
+    },
+    {
+        title: '唤起 app',
+        items: [
+            {
+                text: 'ab',
+                link: 'ab://123',
+                desc: '',
+            },
             // {
             //     text: "-------------------------",
-			// 	link: "javascript:void(0);",
+            // 	link: "javascript:void(0);",
             // }
-        ]
-	},
-	{
-		title: "三方在线工具",
-		items: [
-			{
-				text: "在线Websocket测试",
-				link: "http://coolaf.com/tool/chattest"
-			},
-			{
-				text: "缓动函数效果查询",
-				link: "https://easings.net/cn"
-			},
-			{
-				text: "直播源",
-				link: "https://github.com/ziliudi/ziliudi"
-			},
-			{
-				text: "ios设备UDID",
-				link: "https://www.pgyer.com/tools/udid"
-			},
-			{
-				text: "trackerslist",
-				link: "https://github.com/ngosang/trackerslist"
-			},
-			{
-				text: "TrackersListCollection",
-				link: "https://github.com/XIU2/TrackersListCollection"
-			}
-		],
-	}
-]
+        ],
+    },
+    {
+        title: '三方在线工具',
+        items: [
+            {
+                text: '在线Websocket测试',
+                link: 'http://coolaf.com/tool/chattest',
+            },
+            {
+                text: '缓动函数效果查询',
+                link: 'https://easings.net/cn',
+            },
+            {
+                text: '直播源',
+                link: 'https://github.com/ziliudi/ziliudi',
+            },
+            {
+                text: 'ios设备UDID',
+                link: 'https://www.pgyer.com/tools/udid',
+            },
+            {
+                text: 'trackerslist',
+                link: 'https://github.com/ngosang/trackerslist',
+            },
+            {
+                text: 'TrackersListCollection',
+                link: 'https://github.com/XIU2/TrackersListCollection',
+            },
+        ],
+    },
+];
